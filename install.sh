c65ade809f35e0c51d00e37ebec59f855e648fe9
John <king.johnf@gmail.com>
2024-05-06 10:47:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+if [ ! -d "$HOME/dotfiles" ]; then
+  git clone https://github.com/johnfking/dotfiles.git "$HOME/dotfiles"
+fi
+
+if ! grep -q "source ~/dotfiles/.bashrc" "$HOME/.bashrc"; then
+  echo "Appending dotfiles .bashrc to the current .bashrc..."
+  echo -e "\n# Source dotfiles .bashrc\nsource \$HOME/dotfiles/.bashrc" >> "$HOME/.bashrc"
+fi
+
+source "$HOME/.bashrc"
